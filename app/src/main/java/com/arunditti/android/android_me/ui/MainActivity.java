0b189171296369dc50852dbac9599e7744e5b12a
arunditti <arunditti@gmail.com>
2018-06-06 11:01:09 -0400

    Create and display  MasterListFragment

--- app/src/main/java/com/arunditti/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/arunditti/android/android_me/ui/MainActivity.java
@@ -14,29 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Only create new fragments when there is no previously saved state
-        if(savedInstanceState == null) {
-
-            // Create a new BodyPartFragment instance and display it using the FragmentManager
-            BodyPartFragment headFragment = new BodyPartFragment();
-            headFragment.setImageIds(AndroidImageAssets.getHeads());
-            // Set the list of image id's for the head fragment and set the position to the second image in the list
-            headFragment.setListIndex(1);
-
-            //Use a FragmentManager and transaction to add the fragment to the screen
-            FragmentManager fragmentManager = getSupportFragmentManager();
-
-            //Fragment transaction
-            fragmentManager.beginTransaction().add(R.id.head_container, headFragment).commit();
-
-            BodyPartFragment bodyFragment =  new BodyPartFragment();
-            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
-            fragmentManager.beginTransaction().add(R.id.body_container, bodyFragment).commit();
-
-            BodyPartFragment legFragment = new BodyPartFragment();
-            legFragment.setImageIds(AndroidImageAssets.getLegs());
-            fragmentManager.beginTransaction().add(R.id.leg_container, legFragment).commit();
-        }
-
     }
 }
--- app/src/main/java/com/arunditti/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/arunditti/android/android_me/ui/MasterListFragment.java
@@ -1,42 +1,42 @@
-//package com.arunditti.android.android_me.ui;
-//
-//import android.os.Bundle;
-//import android.support.v4.app.Fragment;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.GridView;
-//
-//import com.arunditti.android.android_me.R;
-//import com.arunditti.android.android_me.data.AndroidImageAssets;
-//
-///**
-// * Created by arunditti on 6/5/18.
-// */
-//
-//public class MasterListFragment extends Fragment {
-//
-//    //Mandatory empty constructor
-//    public MasterListFragment() {
-//    }
-//
-//    //Inflate gridView of all AndroidMe images
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
-//
-//        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
-//
-//        // Get a reference to the GridView in the fragment_master_list xml layout file
-//        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
-//
-//        //Create the adapter. This adapter takes in the context and an ArrayList of all the image resources to display
-//        MasterListAdapter mMasterListAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
-//
-//        //Set the adapter on the GridView
-//        gridView.setAdapter(mMasterListAdapter);
-//
-//        //Return the rootView
-//        return rootView;
-//    }
-//}
-//
+package com.arunditti.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.arunditti.android.android_me.R;
+import com.arunditti.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by arunditti on 6/5/18.
+ */
+
+public class MasterListFragment extends Fragment {
+
+    //Mandatory empty constructor
+    public MasterListFragment() {
+    }
+
+    //Inflate gridView of all AndroidMe images
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        // Get a reference to the GridView in the fragment_master_list xml layout file
+        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
+
+        //Create the adapter. This adapter takes in the context and an ArrayList of all the image resources to display
+        MasterListAdapter mMasterListAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+        //Set the adapter on the GridView
+        gridView.setAdapter(mMasterListAdapter);
+
+        //Return the rootView
+        return rootView;
+    }
+}
+
